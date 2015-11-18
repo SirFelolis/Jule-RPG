45e1a170436c86d3b05d5882bf672b170f368b9c
Oliver Thunæs <Felolis30@gmail.com>
2015-11-18 16:52:26 +0100

    La til simpel fiende script

---
+++ Assets/Scripts/Enemy Behaviors/EnemyMoveBehavior.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMoveBehavior : AbstractEnemyBehavior
+{
+    private float tileSize = 4;
+    private float stride = 1;
+    private float step;
+
+    void FixedUpdate()
+    {
+        step = tileSize * stride;
+
+        var pos = transform.position;
+
+        pos.x += step * -1.0f;
+
+        transform.position = pos;
+
+    }
+}
