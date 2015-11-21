cd07e0b41900cfd9549cba9a0e185a7ad21ba8bd
Oliver Thunæs <Felolis30@gmail.com>
2015-11-21 14:15:10 +0100

    La inn bevegelse på fiendene

--- Assets/Scripts/Enemy Behaviors/EnemyMoveBehavior.cs
+++ Assets/Scripts/Enemy Behaviors/EnemyMoveBehavior.cs
@@ -3,19 +3,55 @@ using System.Collections;
 
 public class EnemyMoveBehavior : AbstractEnemyBehavior
 {
-    private float tileSize = 4;
-    private float stride = 1;
-    private float step;
+    private int tileSize = 4;
+    private int step;
+    public int stride = 2;
+    public int moveChance = 10;
 
     void FixedUpdate()
     {
         step = tileSize * stride;
 
-        var pos = transform.position;
+        if (Random.Range(-moveChance, moveChance) == 0)         // Flytt opp
+        {
+            MoveUp();
+        }
+        else if (Random.Range(-moveChance, moveChance) == 1)    //Flytt ned
+        {
+            MoveDown();
+        }
+        else if (Random.Range(-moveChance, moveChance) == 2)    // Flytt høyre
+        {
+            MoveRight();
+        }
+        else if (Random.Range(-moveChance, moveChance) == 3)    // Flytt venstre
+        {
+            MoveLeft();
+        }
+        else if (Random.Range(-moveChance, moveChance) == 4)    // Gjør ingenting
+        {
+            Debug.Log("Do nothing");
+        }
 
-        pos.x += step * -1.0f;
+    }
 
-        transform.position = pos;
+    void MoveUp()
+    {
+        transform.Translate(new Vector3(0, step, 0));
+    }
 
+    void MoveDown()
+    {
+        transform.Translate(new Vector3(0, -step, 0));
+    }
+
+    void MoveRight()
+    {
+        transform.Translate(new Vector3(step, 0, 0));
+    }
+
+    void MoveLeft()
+    {
+        transform.Translate(new Vector3(-step, 0, 0));
     }
 }
